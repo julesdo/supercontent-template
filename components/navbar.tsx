923eb4201eccf3bd90de140fe1da3fcc887bc880
julesdo <julescamilledore@gmail.com>
2023-08-30 13:52:46 +0200

    maj 1.3

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = async ({storeId}:{storeId:string}) => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">SUPERTRIP</p>
+            <p className="font-bold text-xl">SUPERDEMO</p>
           </Link>
           <MainNav storeId={storeId} data={categories} />
           <NavbarActions />
